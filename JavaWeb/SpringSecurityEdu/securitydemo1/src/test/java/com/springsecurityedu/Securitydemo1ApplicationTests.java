7a237c974dd67d21c29ff99bb17c1ba34ff4873b
wywxhymy <1015162065@qq.com>
2022-01-17 10:08:31 +0800

    first commit

---
+++ JavaWeb/SpringSecurityEdu/securitydemo1/src/test/java/com/springsecurityedu/Securitydemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitydemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
